83ba33f554288a9576d82b1b73f840b1551208e2
Shubham Vishwakarma <127945668+Shub26vish@users.noreply.github.com>
2024-12-05 21:59:16 +0530

    Strings file added

---
+++ Strings/script.js
@@ -0,0 +1,47 @@
+//let name = "Shubham"  //Double Quote
+// console.log(name.length)
+
+//let animal = 'Dog'  //Single Quote
+// console.log(animal)
+
+
+// Template literal
+// let name2 = "Shubham";
+// let age = 20;
+
+// let sentence = `The ${name2} age is ${age}`
+// console.log(sentence)
+
+// Strings Methods
+
+let name3 = "Shubham"
+console.log(name3.length)
+
+let name4 = "Shubham"
+console.log(name4.toUpperCase())
+
+let name5 = "Shubham"
+console.log(name5.toLowerCase())
+
+let name6 = "shivam"
+console.log(name6.slice(2,4))
+
+let name7 = "shivam"
+console.log(name7.slice(2))
+
+let name8 = "Shivam Bhai"
+let newname = name8.replace("Bhai","Dost")
+console.log(newname)
+
+let name9 = "Shivam"
+let name10 = "Sourabh"
+let nameshort = name9.concat(name10," yes")
+console.log(nameshort)
+
+let name11 = "    Shivam"
+let nameshort1 = name11.trim()
+console.log(nameshort1)
+
+let name12 = "Shivam"
+console.log(name12[0])
+console.log(name12[1])
--- loops_&_functions/script.js
+++ loops_&_functions/script.js
@@ -26,9 +26,44 @@ console.log("Loops & Functions");
 // }
 
 // Do while loop
-let n3 = parseInt(prompt("Enter Number"));
-let b = 0;
-do {
-  console.log(b);
-  b++;
-} while (b < n3);
+// let n3 = parseInt(prompt("Enter Number"));
+// let b = 0;
+// do {
+//   console.log(b);
+//   b++;
+// } while (b < n3);
+
+// Practice Questions : -
+// Question 1 : - Write a program to print the marks of the students in an object using for loop
+// obj = { shubham : 98, rohan:87}
+
+// let marks = {
+//   shubham: "98",
+//   rohan: "87",
+// };
+
+// for (let i = 0; i < Object.keys(marks).length; i++) {
+//   console.log(
+//     "The marks of " +
+//       Object.keys(marks)[0] +
+//       " are " +
+//       marks[Object.keys(marks)[0]]
+//   );
+// }
+
+// Question 2 : - Write a number to check whether user has entered the right number.
+// let cn = 43
+// let i
+//  while(i != cn){
+//   console.log("Try again")
+//   i = parseInt(prompt("Enter Number"))
+//  }
+//  console.log("You have entered the right number")
+
+// Queston 4 : - write a four number and find mean of them.
+
+// const mean = (a, b, c, d) => {
+//   return (a + b + c + d) / 4;
+// };
+
+// console.log(mean(4, 5, 6, 7));
