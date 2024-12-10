a671c82af4c197693fd378b07d79fea62126791e
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2024-12-10 15:54:21 +0530

    array methods added

--- Arrays/Script.js
+++ Arrays/Script.js
@@ -1,1 +1,72 @@
-console.log("Array")
+console.log("Array");
+
+// Accessing the vlaues
+let number = [1, 2, 3, 4, 5];
+// console.log(number)
+// console.log(number[0])
+// console.log(number[1])
+// console.log(number[2])
+// console.log(number[3])
+
+// Finding the length of the number
+// console.log(number.length)
+
+// Changing the value of the current array
+// console.log(number[2]=8)
+// console.log(number)
+
+// Methods in string
+// let b = number.toString()
+// console.log(b)
+
+// Method join
+// let b2 = number.join("_")
+// console.log(b2)
+
+// Pop Method in javascript
+// let b3 = number.pop()
+// console.log(b3)
+// console.log(number)
+
+// push method in javascript
+// let c = number.push(53)
+// console.log(c)
+// console.log(number)
+
+// Shift method in javascript
+// let c = number.shift()
+// console.log(c)
+// console.log(number)
+
+// Shift method in javascript
+// let d = number.shift();
+// console.log(d);
+// console.log(number);
+
+// Unshift method
+// let e = number.unshift(53);
+// console.log(e);
+// console.log(number);
+
+// delete method
+// let f = delete number[1];
+// console.log(f);
+// console.log(number);
+// console.log(number[1]);
+
+
+// reverse methos
+// console.log(number.reverse())
+
+// Sort Method
+// let a = ["a","c","b","e","d"]
+// console.log(a.sort())
+
+// Splice Method
+// const number1 = [1,2,3,4,5]
+// console.log(number1.splice(2,1,23,24))
+// console.log(number1)
+
+// Slice Method
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// console.log(fruits.slice(1,4))
