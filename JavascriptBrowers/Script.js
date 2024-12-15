dc3948207a1a036dcd29049bfa380d0b0818f193
Shubham Vishwakarma <127945668+Shub26vish@users.noreply.github.com>
2024-12-15 19:03:13 +0530

    Brwoser Javascript

---
+++ JavascriptBrowers/Script.js
@@ -0,0 +1,9 @@
+// console.log("Helo World")
+// console.assert("Hello" === true)
+// console.assert("Hello" == false)
+
+// const mobile1 = {name:"Iphone", model:"16"}
+// const mobile2 = {name:"Samsung", model:"s24"}
+// console.table([mobile1,mobile2])
+
+alert("Your Script Works")
