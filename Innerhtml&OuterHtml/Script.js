53a5080001277f7bffda37403b4b967ad136d22f
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2025-01-03 14:45:19 +0530

    innerhtml and outer html code added

---
+++ Innerhtml&OuterHtml/Script.js
@@ -0,0 +1,15 @@
+let x = document.getElementsByTagName("span")[0];
+console.log(x);
+
+let y = document.getElementsByTagName("span")[0];
+console.dir(x);
+
+console.log(document.body.firstChild.nodeName);
+console.log(document.body.firstElementChild.nodeName);
+
+first.innerHTML;
+first.innerHTML = "<i>hey I am italic</i>";
+
+document.body.firstChild.data;
+console.log(document.body.textContent);
+first.hidden = false;
